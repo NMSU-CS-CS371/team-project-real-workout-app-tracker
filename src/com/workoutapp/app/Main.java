ce1c79e5418d01074ac97b0f3faeda9fd0a15c6e
mk-cs1404 <mtkirkes@nmsu.edu>
2026-03-30 08:09:39 -0600

    Issue #13 Initial UI Update

--- src/com/workoutapp/app/Main.java
+++ src/com/workoutapp/app/Main.java
@@ -21,45 +21,6 @@ public class Main extends Application{
     }
     public static void main(String[] args) {
 
-        // initialize services
-        String profileName = "test_prof"; // test profile name
-        ProfileService profileService = new ProfileService();
-        ExerciseService exerciseService = new ExerciseService(profileName);
-        RoutineService routineService = new RoutineService(profileName);
-        CalendarService calendarService = new CalendarService(profileName);
-
-
-        // create some exercises
-        Exercise pushUp = new Exercise("Push-Up", "description", ExerciseType.CHEST);
-        exerciseService.addExercise(pushUp);
-        Exercise squat = new Exercise("Squat", "description", ExerciseType.LEGS);
-        exerciseService.addExercise(squat);
-        Exercise plank = new Exercise("Plank", "description", ExerciseType.CORE);
-        exerciseService.addExercise(plank);
-
-        // create a routine and add exercises to it
-        Routine test = new Routine("test");
-        routineService.addRoutine(test);
-
-        routineService.addExercise("test", pushUp);
-        routineService.addExercise("test", squat);
-        routineService.addExercise("test", plank);
-
-        // refetch the routine to ensure it has all exercises
-        test = routineService.findRoutine("test");
-
-        System.out.println(test.toString());
-
-        Workout workout = new Workout(test);
-        System.out.println(workout.toString());
-
-
-
-        // create calendar event with the workout
-        CalendarEvent event = new CalendarEvent(LocalDateTime.now(), workout, "Demo workout session");
-        event.toString();
-        calendarService.addEvent(event);
-
         launch(args);
     }    
 }
